24749592dfd847171de8a2333f62552a4a760010
LaFolle <fghjklp@gmail.com>
2013-06-21 15:10:26 +0530

    merge sort

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,88 +1,51 @@
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-#include <deque>
-#include <stack>
-#include <bitset>
-#include <algorithm>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <sstream>
-#include <iostream>
 #include <iomanip>
 #include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
 #include <iterator>
-#include <queue>
-#include <cassert>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-#define pl cout<<endl;
-#define all(v) v.begin(),v.end()
-#define sz(v) v.size()
-
-
 typedef vector<int> vi;
-typedef vector<vector<int> > vvi;
-typedef vector<string> vs;
 
-
-// inplace sorting of arrays
-void merge(vi &v,int l, int p, int h)
+void merge(int lower, int mid, int upper, vi &v)
 {
-  int i,r,j,k=l,m=p-l+1,n=h-p,e=m+n;
-  vi L(v.begin()+l,v.begin()+p+1),R(v.begin()+p+1,v.begin()+h+1);
-
-  for(l=0,r=0;l<m&&r<n;k++){
-    if(L[l]<R[r]){
-      v[k]=L[l];
-      ++l;
+  int i,j,k,p;
+  vi m(upper-lower+1);
+  for(k=0,i=lower,j=mid+1;i<=mid && j<=upper;){
+    if(v[i]<v[j]){
+      m[k++]=v[i];
+      i++;
     }
-    else{
-      v[k]=R[r];
-      ++r;
+    if(v[j]<=v[i]){
+      m[k++]=v[j];
+      j++;
     }
   }
+  if(i==mid+1 and j==upper+1) return;
+  if(i<=mid) for(p=i;p<=mid;p++) m[k++]=v[p];
+  if(j<=upper) for(p=j;p<=upper;p++) m[k++]=v[p];
+  for(j=0,i=lower;j<k;)v[i++]=m[j++];
+}
 
-  if(l==m&&r!=n){
-    for(i=r;i<n&&k<e;i++,k++)
-      v[k]=R[i];
-  }
-  if(l!=m&&r==n){
-    for(i=l;i<m&&k<e;i++,k++){
-      v[k]=L[i];
-    }
+void mergesort(int lower, int upper, vi &v,int depth)
+{
+  if(lower<upper){
+    int mid = (lower+upper)>>1;
+    mergesort(lower, mid, v,depth+1);
+    mergesort(mid+1, upper, v, depth+1);
+    merge(lower, mid, upper, v);
   }
 }
 
-void mergesort(vi &v,int l,int h)
-{
-  int p;
-  if(l<h){
-    p=(l+h)/2;
-    mergesort(v,l,p);
-    mergesort(v,p+1,h);
-    merge(v,l,p,h);
-  }
-}
 
 int main()
 {
-  int t,i;
-  scanf("%d",&t);
-  while(t--){
-    int n;
-    scanf("%d", &n);
-    vi v(n);
-    for(i=0;i<n;i++) scanf("%d",&v[i]);
-    mergesort(v,0,n-1);
-    copy(all(v),ostream_iterator<int>(cout," "));pl;
-  }
-  return 0;
+  int n,i;
+  int A[] = {3,5,1,6,7,9,8};
+  n = sizeof(A)/sizeof(A[0]);
+  vi v(A, A+n);
+  mergesort(0, n-1, v,0);
+  for(i=0;i<n;i++)cout<<v[i]<<endl;
 }
-
